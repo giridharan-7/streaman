f6080694caf99360c8367c694b8dd9f65f7eb871
Giridharan <giridharanias7@gmail.com>
2025-01-19 20:50:47 +0530

    Introduced pricing home contact routes to streaman

--- src/app/(website)/_components/navbar.tsx
+++ src/app/(website)/_components/navbar.tsx
@@ -1,16 +1,36 @@
-import { MenuIcon } from 'lucide-react'
+import { Link as LucideLink, MenuIcon } from 'lucide-react'
+import Link from 'next/link'
+import Image from 'next/image'
 import React from 'react'
 
 type Props = {}
 
 const LandingPageNavBar = () => {
   return (
-    <div className='flex justify-between items-center'>
+    <nav className='flex justify-between items-center'>
       <div className='text-3xl font-semibold flex items-center gap-x-3'>
         <MenuIcon className="w-6 h-6" />
-        
+        <Image
+          alt='logo'
+          src='/streaman-logo.svg'
+          width={40}
+          height={40}
+          priority
+        />
+        <span>Streaman</span>
       </div>
-    </div>
+      <div className='hidden gap-x-10 items-center lg:flex'>
+        <Link href='/' className='bg-[#7320DD] py-2 px-5 font-semibold text-lg rounded-full hover:bg-[#7320DD]/80'>
+          <span>Home</span>
+        </Link>
+        <Link href='/' className='py-2 px-5 font-semibold text-lg hover:text-[#7320DD]'>
+          <span>Pricing</span>
+        </Link>
+        <Link href='/' className='py-2 px-5 font-semibold text-lg hover:text-[#7320DD]'>
+          <span>Contact</span>
+        </Link>
+      </div>
+    </nav>
   )
 }
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,8 +28,9 @@ export default function RootLayout({
             <ThemeProvider
               attribute="class"
               defaultTheme="dark"
-              enableSystem={false}
+              enableSystem
               disableTransitionOnChange
+              storageKey="streaman-theme"
             >
               {children}
             </ThemeProvider>
